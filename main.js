52608341ca6866d8815cfb28b65c415803cc5699
KuroiMatoO <kuroimato.o@gmail.com>
2024-07-12 15:46:44 +0300

    filtering placeholder

--- main.js
+++ main.js
@@ -8,170 +8,306 @@ let resultContainer = document.querySelector('#result-container')
 let RSS;
 let typingTimeout;
 
-function parseRSS() {
-    if (inputContainer.className == "minimized") {
-        inputContainer.className = "";
-        resultContainer.className = "";
-    } else {
-        inputContainer.className = "minimized";
-        resultContainer.className = "expanded";
-    }
-  }
+button.addEventListener("click", ParseRSS);
 
-function buttonToggle(){
-    if (input.value){
-        button.className = 'button';
-    } else {
-        button.className = 'button disabled';
-    }
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    ParseRSS();
+  }
+});
+
+input.addEventListener('input', () => {
+  clearTimeout(typingTimeout);
+  typingTimeout = setTimeout(buttonToggle, 100);
+});
+
+
+function classToggle() {
+  if (inputContainer.className !== "minimized") {
+    inputContainer.className = "minimized";
+    resultContainer.className = "expanded";
+  }
+}
+
+
+
+function buttonToggle() {
+  if (input.value) {
+    button.className = 'button';
+  } else {
+    button.className = 'button disabled';
+  }
 }
 
 
 async function Loader() {
   try {
-      const loader = document.querySelector('.loader')
-      const loaderBg = document.querySelector('.loader_bg')
-      loader.className = 'loader';
-      loaderBg.className = 'loader_bg';
-      var timer = setInterval(checkRSS, 1000);
-      function checkRSS() {
-          if (RSS) {
-              clearInterval(timer);
-              loader.className = 'loader hidden';
-              loaderBg.className = 'loader_bg hidden';
-              console.log(RSS)
+    const loader = document.querySelector('.loader')
+    const loaderBg = document.querySelector('.loader_bg')
+    loader.className = 'loader';
+    loaderBg.className = 'loader_bg';
+    var timer = setInterval(checkRSS, 1000);
+    function checkRSS() {
+      if (RSS) {
+        clearInterval(timer);
+        loader.className = 'loader hidden';
+        loaderBg.className = 'loader_bg hidden';
+        // console.log(RSS)
 
-          } else {
-              console.log('something wrong')
-              console.log(RSS)
-          }
+      } else {
+        console.log('loading')
+        // console.log(RSS)
       }
+    }
   } catch (error) {
-      console.error(error);
+    console.error(error);
+    let timeout = 'timed out'
+    if (error.includes(timeout)){
+      console.log('TIMEOUT')
+    }
   }
 
 }
 
-button.addEventListener("click", parseRSS);
-
-input.addEventListener('input', () => {
-    clearTimeout(typingTimeout);
-    typingTimeout = setTimeout(buttonToggle, 500);
-});
 
 
 //Proxy
-const CORS_PROXY = "https://api.allorigins.win/raw?url="
+// const CORS_PROXY = "https://api.allorigins.win/raw?url="
+const CORS_PROXY = "https://corsproxy.io/?"
 
-let parser = new RSSParser();
-parser.parseURL(CORS_PROXY + 'https://www.animenewsnetwork.com/all/rss.xml?ann-edition=us', function(err, RSSData) {
-  if (err) throw err;
-  console.log(RSSData)
-  RSS = RSSData;
-  
-function createFeedItem(item) {
-    const article = document.createElement('article');
-    article.className = 'feed-item';
-    
-    article.onclick = function() {
-        window.open(item.link, '_blank');
-    };
-    
-    const title = document.createElement('h2');
-    title.textContent = item.title;
-    article.appendChild(title);
-  
-    const infoRow = document.createElement('div');
-    infoRow.className = 'info-row';
-    
-    const pubDate = new Date(item.pubDate);
-    const formattedDate = pubDate.toLocaleDateString('en-US', { 
-      weekday: 'short', 
-      day: 'numeric', 
-      month: 'short', 
-      year: 'numeric'
-    });
-    
-    if (item.categories !== undefined){
-        if (Object.keys(item.categories).length > 1){
-            infoRow.textContent = `${formattedDate} | ${item.categories.join(', ')}`;
-        } else {
-            infoRow.textContent = `${formattedDate} | ${item.categories}`;
-        }
-    } else {
-        infoRow.textContent = `${formattedDate}` 
+let parser = new RSSParser({
+  headers: {
+    'Accept': 'application/rss+xml, text/xml',
+  },
+  customFields: {
+    item: ['media:content'],
+  },
+  requestOptions: {
+    headers: {
+      'Accept': 'application/rss+xml, text/xml',
     }
-   
-    article.appendChild(infoRow);
-  
-    const content = document.createElement('p');
-    content.textContent = item.contentSnippet;
-    article.appendChild(content);
-  
-    return article;
   }
-  
-  function populateFeed(RSSData, containerId) {
-    const container = document.getElementById(containerId);
-    
-    RSSData.items.forEach(item => {
-      const feedItem = createFeedItem(item);
-      container.appendChild(feedItem);
-    });
-  }
+});
 
-  Loader()
-  populateFeed(RSSData, 'results');
-})
+function ParseRSS() {
+  RSS = '';
+  if (input.value) {
+    URL = input.value;
+    classToggle();
+    Loader()
+
+    parser.parseURL(CORS_PROXY + URL, function (err, RSSData) {
+      if (err) throw err;
+      console.log(RSSData)
+      RSS = RSSData;
+
+      let feedItem = document.querySelectorAll('.feed-item')
+      if (feedItem){
+        for (let i = 0; i < feedItem.length; i++){
+          feedItem[i].remove()
+        }
+      }
+      function createFeedItem(item) {
+        const article = document.createElement('article');
+        article.className = 'feed-item';
+
+        article.onclick = function () {
+          let link = item.enclosure?.url || item.link;
+          console.log(link)
+          window.open(link, '_blank');
+        }
+
+        const title = document.createElement('h2');
+        title.textContent = item.title;
+        article.appendChild(title);
+
+        const infoRow = document.createElement('div');
+        infoRow.className = 'info-row';
+
+        const tags = document.createElement('span');
+        tags.className = 'tags';
+
+        const pubDate = new Date(item.pubDate);
+        const formattedDate = pubDate.toLocaleDateString('en-US', {
+          weekday: 'short',
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric'
+        });
+
+       
+        if (item.categories !== undefined) {
+          if (Object.keys(item.categories).length > 1) {
+            infoRow.textContent = `${formattedDate} | `;
+            item.categories.forEach((category) => {
+              const tag = document.createElement('span');
+              tag.className = 'tag';
+              tag.textContent = `[${category}] `;
+              tags.appendChild(tag);
+              filterTags(item.categories)
+            })
+          } else {
+            infoRow.textContent = `${formattedDate} | `;
+            const tag = document.createElement('span');
+            tag.className = 'tag';
+            tag.textContent = `[${item.categories}] `;
+            tags.appendChild(tag);
+            filterTags(item.categories)
+
+          }
+        } else {
+          infoRow.textContent = `${formattedDate}`
+          allTags.clear();
+        }
+        
+    
+        article.appendChild(infoRow);
+        infoRow.appendChild(tags);
+
+       
+
+
+        const content = document.createElement('p');
+        content.textContent = item.contentSnippet;
+        article.appendChild(content);
+
+        return article;
+      }
+
+      function populateFeed(RSSData, containerId) {
+        const container = document.getElementById(containerId);
+
+        RSSData.items.forEach(item => {
+          const feedItem = createFeedItem(item);
+          container.appendChild(feedItem);
+        });
+      }
+
+      populateFeed(RSSData, 'results');
+      fetchFilterTags()
+    })
+  }
+}
+
+
+//Filtering
+let allTags = new Set();
+
+function filterTags(categories) {
+
+  if (categories !== undefined) {
+    categories.forEach((category) => {
+      allTags.add(category);
+      // console.log(categories)
+    })
+  }
+  // console.log(allTags)
+}
+
+
+let filterBtn = document.createElement('div');
+let filterList = document.createElement('div');
+
+
+function fetchFilterTags(){
+  filterList.innerHTML = '';
+
+  if (document.querySelector('.filter-btn') == undefined){
+    // filterBtn = document.createElement('div');
+    filterBtn.className = 'filter-btn';
+    inputContainer.appendChild(filterBtn)
+  }
+  if (document.querySelector('.filter-list') == undefined){
+    filterList.className = 'filter-list invisible';
+    resultContainer.before(filterList);
+  }
+      
+  
+    
+    allTags.forEach((tag) => {
+      let filterItem = document.createElement('div')
+      filterItem.className = 'filter-item';
+      filterList.appendChild(filterItem)
+      
+      let filterCheckBox = document.createElement('input')
+      filterCheckBox.type = 'checkbox';
+      filterCheckBox.className = 'filter-checkbox';
+      filterItem.appendChild(filterCheckBox)
+  
+      let filterTag = document.createElement('span');
+      filterTag.className = 'filter-tag';
+      filterTag.textContent = tag;
+      filterItem.appendChild(filterTag);
+    })
+}
+
+filterBtn.addEventListener('click', filterToggle);
+
+function filterToggle(){
+  if (filterList.className == 'filter-list invisible'){
+    filterList.className = 'filter-list';
+  } else {
+    filterList.className = 'filter-list invisible';
+  }
+}
 
 
 
 
 //THEME
 
-const themeToggle = document.getElementById('theme-toggle');
-const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+let themeToggle = document.querySelector('#theme-toggle');
 let themeIMG = document.querySelector('#theme-toggle img');
+let prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+
+themeToggle.addEventListener('click', toggleTheme);
+prefersDarkScheme.addEventListener('change', handleColorSchemeChange);
+
+// Detect OS theme
+function handleColorSchemeChange(event) {
+  let newTheme;
+  if (event.matches) {
+    newTheme = 'dark';
+  } else {
+    newTheme = 'light';
+  }
+  setTheme(newTheme);
+}
 
 function setTheme(theme) {
   document.documentElement.setAttribute('data-theme', theme);
   localStorage.setItem('theme', theme);
   themeIMG.src = `img/${theme}.png`;
-
 }
 
 function loadTheme() {
-  const savedTheme = localStorage.getItem('theme');
+  let savedTheme = localStorage.getItem('theme');
   if (savedTheme) {
+    console.log('Using saved theme');
     setTheme(savedTheme);
-    themeIMG.src = `img/${savedTheme}.png`;
-  } else if (prefersDarkScheme.matches) {
-    setTheme('dark');
   } else {
-    setTheme('light');
+    console.log('Using system preference');
+    handleColorSchemeChange(prefersDarkScheme);
   }
 }
 
 function toggleTheme() {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+  console.log(allTags)
+
+  let currentTheme = document.documentElement.getAttribute('data-theme');
+  let newTheme = currentTheme === 'light' ? 'dark' : 'light';
   setTheme(newTheme);
-  if (newTheme === 'dark'){
+  if (newTheme === 'dark') {
     themeIMG.src = "img/dark.png";
   } else {
     themeIMG.src = "img/light.png"
   }
 }
 
-// Initialize theme
+
+
 loadTheme();
-
-// Event listener for theme toggle button
-themeToggle.addEventListener('click', toggleTheme);
-
-// Listen for OS theme changes
-prefersDarkScheme.addEventListener('change', (e) => {
-  const newTheme = e.matches ? 'dark' : 'light';
-  setTheme(newTheme);
- 
-});
+//TODO
+//Sort by tag
+//Timeour error indication
